0ceca761ae97a0bacf3487abb59752be881b47b8
moro <moro@moro.local>
2017-06-22 18:06:14 +0300

    add start all command, add new patterns

--- hs-proxy.config.js
+++ hs-proxy.config.js
@@ -2,61 +2,81 @@ var i18Host = 'http://localhost:6060';
 var apiHost = 'http://df-testserver4/';
 //var apiHost = 'http://10.1.19.12/';
 var staticHost = 'http://localhost:8080/';
-var staticRedirectHost = 'http://localhost:5000';
+var staticOldRedirectHost = 'http://localhost:5000';
+var staticRedirectHost = 'http://localhost:6500';
 
 module.exports = {
-  'proxy': {
-    'port': 8003,
-    'static': {
-      'target': staticHost,
-      'removePrefix': 'hs',
-      'requestTimeout': 1200000,
-      'redirect': [{
-          'from': 'scheduling.min.js',
-          'to': 'scheduling/config.js'
-        }, {
-          'from': 'new-timeoff-approvals.min.master.+.js',
-          'to': 'approve-main.js'
+    proxy: {
+        port: 8003,
+        static: {
+            target: staticHost,
+            removePrefix: 'hs',
+            requestTimeout: 1200000,
+            redirect: [
+                {
+                    from: 'scheduling.min.js',
+                    to: 'scheduling/config.js'
+                },
+                {
+                    from: 'new-timeoff-approvals.min.master.+.js',
+                    to: 'approve-main.js'
+                },
+                {
+                    from: 'new-forecast.min.master.+.js',
+                    to: 'forecast/forecast.config.js'
+                },
+                {
+                    from: '.*update.js$',
+                    replace: '/',
+                    to: staticRedirectHost
+                },
+                {
+                    from: '.*update.json$',
+                    replace: '/',
+                    to: staticRedirectHost
+                },
+                {
+                    from: '/apps/.*[.js|.css|.json|.woff2|.woff|.ttf]',
+                    replace: [
+                        '/',
+                        {
+                            from: new RegExp('bundle[.].{1,}[.]js'),
+                            to: 'bundle.dev.js'
+                        },
+                        {
+                            from: new RegExp('share-employees'),
+                            to: 'modules/share-employees'
+                        }
+                    ],
+                    to: staticOldRedirectHost
+                },
+                {
+                    from: '/apps/dist/vendor/.*[.js|.css|.json|.woff2|.woff|.ttf]',
+                    to: staticRedirectHost,
+                    replace: '/'
+                },
+                {
+                    from: '/apps/dist/module-launcher/.*[.js|.css|.json|.woff2|.woff|.ttf]',
+                    to: staticRedirectHost,
+                    replace: '/'
+                },
+                {
+                    from: '/templates/green/js/logbook/.*[.js|.css|.json|.woff2|.woff|.ttf]',
+                    replace: '/templates/green/js/logbook/dist/',
+                    to: staticRedirectHost
+                },
+                {
+                    from: '/spring/i18n/resources',
+                    replace: '/',
+                    to: i18Host
+                }
+            ],
+            exclude: ['hotschedules.js', 'jsp/preload'],
+            include: ['.png$|.js$|.cssv$|.woff$|.css$|i18n/resources|.svg$|.jpg$|.gif$|.html$|.ttf|.ico$|.tpl$|.json$|.woff2$']
         },
-        {
-          'from': 'new-forecast.min.master.+.js',
-          'to': 'forecast/forecast.config.js'
-        }, {
-          'from': '.*update.js$',
-          'replace': '/',
-          'to': staticRedirectHost
-        }, {
-          'from': '.*update.json$',
-          'replace': '/',
-          'to': staticRedirectHost
-        }, {
-          'from': '\/apps\/.*[.js|.css|.json|.woff2|.woff|.ttf]',
-          'replace': ['/', {
-            from: new RegExp('bundle[.].{1,}[.]js'),
-            to: 'bundle.dev.js'
-          }],
-          'to': staticRedirectHost
-        }, {
-          'from': '\/templates\/green\/js\/logbook\/.*[.js|.css|.json|.woff2|.woff|.ttf]',
-          'replace': '\/templates\/green\/js\/logbook\/dist\/',
-          'to': staticRedirectHost
-        }, {
-          'from': '\/spring\/i18n\/resources',
-          'replace': '\/',
-          'to': i18Host
+        dynamic: {
+            target: apiHost,
+            requestTimeout: 1200000
         }
-      ],
-      'exclude': [
-        'hotschedules.js',
-        'jsp/preload'
-      ],
-      'include': [
-        '.png$|.js$|.cssv$|.woff$|.css$|i18n\/resources|.svg$|.jpg$|.gif$|.html$|.ttf|.ico$|.tpl$|.json$|.woff2$'
-      ]
-    },
-    'dynamic': {
-      'target': apiHost,
-      'requestTimeout': 1200000
     }
-  }
-}
+};
--- server/hs-proxy.js
+++ server/hs-proxy.js
@@ -1,127 +1,122 @@
 require('./dns-fix');
 
-var
-  fs = require('fs'),
-  http = require('http'),
-  urlUtils = require('url'),
-  express = require('express'),
-  httpProxy = require('http-proxy'),
-  config = require('./../hs-proxy.config.js'),
-  staticUrlReplace = config.proxy.static.redirect,
-  staticInclude = config.proxy.static.include,
-  staticExclude = config.proxy.static.exclude,
-  staticRemovePrefix = config.proxy.static.removePrefix;
+var fs = require('fs'),
+    http = require('http'),
+    urlUtils = require('url'),
+    express = require('express'),
+    httpProxy = require('http-proxy'),
+    config = require('./../hs-proxy.config.js'),
+    staticUrlReplace = config.proxy.static.redirect,
+    staticInclude = config.proxy.static.include,
+    staticExclude = config.proxy.static.exclude,
+    staticRemovePrefix = config.proxy.static.removePrefix;
 
-var staticProxy = httpProxy
-  .createProxyServer({
+var staticProxy = httpProxy.createProxyServer({
     timeout: config.proxy.static.requestTimeout
-  });
+});
 
 staticProxy.on('error', onError);
 
-var dynamicProxy = httpProxy
-  .createProxyServer({
+var dynamicProxy = httpProxy.createProxyServer({
     timeout: config.proxy.dynamic.requestTimeout
-  });
+});
 
 dynamicProxy.on('error', onError);
 
-http
-  .createServer(proxyRequest)
-  .listen(config.proxy.port);
+http.createServer(proxyRequest).listen(config.proxy.port);
 
 function proxyRequest(req, res) {
-  var currentUrl = urlUtils.parse(req.url);
-  var pathName = currentUrl.path;
+    var currentUrl = urlUtils.parse(req.url);
+    var pathName = currentUrl.path;
 
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "origin, content-type, accept, x-requested-with, Authorization, X-Scheme, X-CSRFToken");
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept, x-requested-with, Authorization, X-Scheme, X-CSRFToken');
 
-  if (match(pathName.replace(/[?].+/g, ''), staticInclude) && !match(pathName, staticExclude)) {
-    var redirectUrl = redirect(req.url, staticUrlReplace, req.query);
+    if (match(pathName.replace(/[?].+/g, ''), staticInclude) && !match(pathName, staticExclude)) {
+        var redirectUrl = redirect(req.url, staticUrlReplace, req.query);
 
-    if (redirectUrl) {
-      console.log('proxy redirect ', req.url, '<=>', redirectUrl);
+        if (redirectUrl) {
+            console.log('proxy redirect ', req.url, '<=>', redirectUrl);
 
-      res.writeHead(302, {
-        'Location': redirectUrl
-      });
+            res.writeHead(302, {
+                Location: redirectUrl
+            });
 
-      res.end();
+            res.end();
+        } else {
+            req.url = req.url.replace(new RegExp(staticRemovePrefix), '');
+            console.log('proxy static ', pathName, '<=>', req.url);
+
+            staticProxy.web(req, res, {
+                target: config.proxy.static.target
+            });
+        }
     } else {
-      req.url = req.url.replace(new RegExp(staticRemovePrefix), '');
-      console.log('proxy static ', pathName, '<=>', req.url);
+        console.log('proxy dynamic', req.url, '<=>', config.proxy.dynamic.target + pathName);
 
-      staticProxy.web(req, res, {
-        target: config.proxy.static.target
-      });
+        dynamicProxy.web(req, res, {
+            target: config.proxy.dynamic.target
+        });
     }
-  } else {
-    console.log('proxy dynamic', req.url, "<=>", config.proxy.dynamic.target + pathName);
-
-    dynamicProxy.web(req, res, {
-      target: config.proxy.dynamic.target
-    });
-  }
 }
 
 function match(path, patterns) {
-  var result = false;
-  patterns = patterns || [];
+    var result = false;
+    patterns = patterns || [];
 
-  for (var i = patterns.length - 1; i >= 0; i--) {
-    if (path.match(new RegExp(patterns[i]))) {
-      result = patterns[i];
-      break;
+    for (var i = patterns.length - 1; i >= 0; i--) {
+        if (path.match(new RegExp(patterns[i]))) {
+            result = patterns[i];
+            break;
+        }
     }
-  }
 
-  return result;
+    return result;
 }
 
 function redirect(path, patterns) {
-  var result, pattern;
-  patterns = patterns || [];
+    var result, pattern;
+    patterns = patterns || [];
 
-  for (var i = patterns.length - 1; i >= 0; i--) {
-    pattern = patterns[i];
+    for (var i = patterns.length - 1; i >= 0; i--) {
+        pattern = patterns[i];
 
-    if (path.match(new RegExp(pattern.from))) {
-      if (~pattern.to.indexOf('http://')) {
-        result = pattern.to + '/' + replace(pattern.replace, path);
-      } else {
-        result = path.replace(new RegExp(pattern.from), pattern.to);
-      }
+        if (path.match(new RegExp(pattern.from))) {
+            if (~pattern.to.indexOf('http://')) {
+                result = pattern.to + '/' + replace(pattern.replace, path);
+            } else {
+                result = path.replace(new RegExp(pattern.from), pattern.to);
+            }
 
-      break;
+            break;
+        }
     }
-  }
 
-  return result;
+    return result;
 }
 
 function replace(replace, path) {
-  var result = path;
+    var result = path;
 
-  if (Array.isArray(replace)) {
-    replace.forEach(function(item){
-        if (typeof item === 'string') {
-          result = result.replace(item, '');
-        } else if (typeof item === 'object'){
-          result = result.replace(item.from, item.to);
-        }
-    });
-  } else if (replace) {
-    result = result.replace(replace, '');
-  }
+    if (Array.isArray(replace)) {
+        replace.forEach(function(item) {
+            if (typeof item === 'string') {
+                result = result.replace(item, '');
+            } else if (typeof item === 'object') {
+                result = result.replace(item.from, item.to);
+            }
+        });
+    } else if (replace) {
+        result = result.replace(replace, '');
+    }
 
-  return result;
+    return result;
 }
 
 function onError(error) {
-  console.error('error', error);
+    console.error('error', error);
 }
 
 console.log('http proxy server has been started on port ' + config.proxy.port);
